4e555009428667f6bef7c838291b9df25102d59e
Kai Cruz <dredoreiruk@aol.com>
2019-01-24 04:55:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ function createRock(x) {
     }
     console.log(rockTop + 22)
     if (rockTop == 378){
-     GAME.removeChild(rockList[i].pop());
+     document.div.removeChild(rockList[i].pop());
     }
   }
 /**
